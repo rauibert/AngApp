d93acc67a5354c5adb59026a8a2034d968f54326
Rauibert <rauibertnf@gmail.com>
2020-06-06 00:10:03 +0200

    Use of ngIf

--- src/app/comics/comics.component.ts
+++ src/app/comics/comics.component.ts
@@ -13,7 +13,8 @@ export class ComicsComponent implements OnInit {
   constructor() {
     this.comics = [
       new Comic('Spiderman Vuelta a casa','Straczynski', 'Romita Jr.', 2001, true),
-      new Comic ('Hasta que las estrellas se congelen','Straczynski', 'Romita Jr.', 2001, true)
+      new Comic ('Hasta que las estrellas se congelen','Straczynski', 'Romita Jr.', 2001, true),
+      new Comic ('Spiderman 2020','Straczynski', 'Romita Jr.', 2020, true)
     ];
   }
 
