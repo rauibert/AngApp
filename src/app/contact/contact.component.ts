8b3bce9f57f55ab3fedbaaf30e3a587f1c9222b2
Rauibert <rauibertnf@gmail.com>
2020-07-05 22:18:30 +0200

    Creating a contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserContact } from '../models/userContact';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public userContact: UserContact;
 
-  constructor() { }
+  constructor() { 
+    this.userContact = new UserContact('','','','');
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log("Submit done");
+    console.log(this.userContact);
   }
 
 }
---
+++ src/app/models/userContact.ts
@@ -0,0 +1,9 @@
+export class UserContact{
+    
+    constructor(
+        public first_name: string,
+        public last_name: string,
+        public email: string,
+        public message: string
+    ){}
+}
